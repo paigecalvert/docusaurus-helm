2de5522dbcef89037aaef4d68d1a7343e7a4811e
Paige Calvert <paige@replicated.com>
2025-09-09 16:16:38 -0600

    test

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Helm',
-  tagline: 'Helm - The Kubernetes Package Manager',
+  tagline: 'The Kubernetes Package Manager',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
