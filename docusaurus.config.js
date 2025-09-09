0d86c3ea54167df9dbf634d3ceb094d149c3e2af
Scott Rigby <scott@r6by.com>
2025-09-09 17:25:06 -0400

    change to test ci
    Signed-off-by: Scott Rigby <scott@r6by.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Helm',
-  tagline: 'Dinosaurs are cool',
+  tagline: 'Helm - The Kubernetes Package Manager',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
