24c787f5e46e5066a639cfc7fb2ff2efbc5a82ea
Paige Calvert <paige@replicated.com>
2025-09-10 09:26:09 -0600

    fix edit page links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -55,7 +55,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/paigecalvert/docusaurus-helm-test',
+            'https://github.com/paigecalvert/docusaurus-helm-test/blob/main/',
         },
         blog: {
           showReadingTime: true,
